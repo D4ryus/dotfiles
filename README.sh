383e05126bfbb528c33577d0526fe2a58aeffa67
d4ryus <w.wackerbauer@yahoo.de>
2015-01-14 15:28:40 +0100

    added ignore option

--- README.sh
+++ README.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 # to link everything just execute this README file
 
+IGNORE="README.sh dwm_6.0.diff"
+
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-for i in $(ls $1); do
+for i in $(ls $DIR); do
+        if [[ $IGNORE =~ .*$i.* ]]; then
+                continue;
+        fi
         if [ -a $HOME/.$i ]; then
                 mv $HOME/.$i $HOME/.$i.backup;
         fi
