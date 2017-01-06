138ef4fe5e73c3c601666029fe022365b499cd3d
d4ryus <d4ryus@openmailbox.org>
2017-01-06 12:21:07 +0100

    emacs: always set scroll/tool-bar-mode to -1

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,9 +18,8 @@
       '(("." . "~/.emacs.d/backup/")))
 
 ;; dont show tool- or scroll-bar
-(when (display-graphic-p)
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1))
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 
 (setq-default
  ;; disable startup message
