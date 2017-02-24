66b91a8f0e150be12e30333e4bf812c32f8892ee
d4ryus <d4ryus@openmailbox.org>
2017-02-24 11:54:58 +0100

    emacs: fixes indentation

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -65,8 +65,8 @@
 ;; use-package
 
 (unless (package-installed-p 'use-package)
-        (package-refresh-contents)
-        (package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (setq use-package-always-ensure t)
 
