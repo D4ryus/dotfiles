8cc8f3cbc5f26d12e89221879caf00e7b7e06a51
d4ryus <d4ryus@openmailbox.org>
2017-08-29 08:44:39 +0200

    emacs: add functions to set c style on buffers

--- emacs.d/d4.el
+++ emacs.d/d4.el
@@ -101,3 +101,21 @@ to clock into or nil"
            (goto-char (cdr entry))
            (org-clock-in))
           (message "no entry found")))))
+
+(defun d4-buffer-mode (buffer-or-name)
+  "return mode of given buffer"
+  (with-current-buffer buffer-or-name major-mode))
+
+(defun d4-filter-buffers-by-mode (mode &optional buffer-list)
+  "return all buffers with given mode"
+  (delq nil
+        (mapcar (lambda (buffer)
+                  (and (eq (d4-buffer-mode buffer) mode) buffer))
+                (or buffer-list (buffer-list)))))
+
+(defun d4-set-style (style &rest modes)
+  "set style to all buffers with given modes"
+  (mapc (lambda (buffer)
+          (with-current-buffer buffer
+            (c-set-style style)))
+        (apply 'append (mapcar 'd4-filter-buffers-by-mode modes))))
