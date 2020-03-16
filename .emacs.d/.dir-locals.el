d21aaebd79cef49fd5ec4a26333a5b4d6eca6ef3
d4ryus <d4ryus@teknik.io>
2020-03-16 19:35:42 +0100

    emacs: Fix dumb typo in .dir-locals.el

--- .emacs.d/.dir-locals.el
+++ .emacs.d/.dir-locals.el
@@ -1,3 +1,3 @@
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
-                     (eval . (put 'use-package 'lisp-indent-function '1)))))
+                     (eval . (put 'use-package 'lisp-indent-function 1)))))
 
