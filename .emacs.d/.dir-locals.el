519994548d7337888f71d6c2bb2bd0b5a9c5d602
d4ryus <d4ryus@teknik.io>
2020-06-10 10:42:20 +0200

    emacs: Elisp uses common-lisp-indent-function

--- .emacs.d/.dir-locals.el
+++ .emacs.d/.dir-locals.el
@@ -1,3 +1,4 @@
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
-                     (eval . (put 'use-package 'lisp-indent-function 1)))))
+                     (eval . (put 'use-package 'lisp-indent-function 1))
+                     (eval . (put 'use-package 'common-lisp-indent-function 1)))))
 
