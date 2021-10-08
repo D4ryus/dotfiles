248c65dab8b501f3c36ad81fe3834d6e138115d4
d4ryus <d4ryus@mailbox.org>
2021-10-08 11:39:29 +0200

    emacs: Merge d4.el -> init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,10 +11,107 @@
 
 (require 'd4-pkg)
 (require 'd4-org)
-(require 'd4)
 (when (file-exists-p (concat local-lisp-directory "d4-local.el"))
   (require 'd4-local))
 (require 'd4-clocking)
+;; --- custom functions
+
+(defun d4-set-background (mode)
+  "set background to given mode which is either 'dark or 'light"
+  (setq frame-background-mode mode)
+  (mapc 'frame-set-background-mode (frame-list))
+  (let ((dark  "#000000")
+        (light "#FFFFFF"))
+    (if (eql mode 'dark)
+        (progn (set-background-color dark)
+               (set-foreground-color light))
+        (progn (set-background-color light)
+               (set-foreground-color dark)))))
+
+(defun d4-buffer-mode (buffer-or-name)
+  "return mode of given buffer"
+  (with-current-buffer buffer-or-name major-mode))
+
+(defun d4-filter-buffers-by-mode (mode &optional buffer-list)
+  "return all buffers with given mode"
+  (delq nil
+        (mapcar (lambda (buffer)
+                  (and (eq (d4-buffer-mode buffer) mode) buffer))
+                (or buffer-list (buffer-list)))))
+
+(defun d4-set-style (style &rest modes)
+  "set style to all buffers with given modes"
+  (mapc (lambda (buffer)
+          (with-current-buffer buffer
+            (c-set-style style)))
+        (apply 'append (mapcar 'd4-filter-buffers-by-mode modes))))
+
+(defun d4-to-bin (number)
+  (apply 'concat
+         (reverse
+          (loop for i to 31
+                collect
+                (format
+                 (if (and (not (eql i 0))
+                          (eql (mod i 4) 0))
+                     "%d "
+                     "%d")
+                 (logand #x1 (lsh number (- i))))))))
+
+(defun d4-to-ascii (number)
+  (apply 'concat
+         (reverse
+          (loop for i to 7
+                collect
+                (format
+                 "%c"
+                 (c-int-to-char
+                  (logand #xff (lsh number (- (* i 8))))))))))
+
+(defun d4-YYY ()
+  (interactive)
+  (insert
+   "#define YYY(__fmt, ...)					/* YYY */\\\n"
+   "	do {							/* YYY */\\\n"
+   "		printf(\"YYY:%s:%s:%d:\" __fmt \"\\n\",		/* YYY */\\\n"
+   "		    __FILE__, __PRETTY_FUNCTION__, __LINE__,	/* YYY */\\\n"
+   "		    ##__VA_ARGS__);				/* YYY */\\\n"
+   "	} while (0)						/* YYY */\n"))
+
+(defun jarvis-copy (&optional text)
+  (interactive)
+  (dbus-call-method
+   :session "net.d4ryus.Jarvis"
+   "/net/d4ryus/Jarvis"
+   "net.d4ryus.Clipboard" "Copy"
+   (or text
+       (and (region-active-p)
+            (buffer-substring-no-properties
+             (region-beginning)
+             (region-end)))
+       (error "Copy requires region or text"))))
+
+(defun jarvis-kill ()
+  (interactive)
+  (let ((text
+          (if (region-active-p)
+              (buffer-substring-no-properties
+               (region-beginning)
+               (region-end))
+              (error "Select text to kill"))))
+    (delete-active-region t)
+    (jarvis-copy text)))
+
+(defun jarvis-paste ()
+  (interactive)
+  (let ((text (dbus-call-method
+               :session "net.d4ryus.Jarvis"
+               "/net/d4ryus/Jarvis"
+               "net.d4ryus.Clipboard" "Paste")))
+    (when (region-active-p)
+      (delete-active-region t))
+    (insert text)))
+
 
 ;; backup settings
 (setq backup-directory-alist
