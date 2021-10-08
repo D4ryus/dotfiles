0142cbafd9fc5baa9fc422c10844f6eaa3899741
d4ryus <d4ryus@mailbox.org>
2021-10-08 11:41:15 +0200

    emacs: Merge d4-org.el -> init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,9 +10,61 @@
 (add-to-list 'load-path local-lisp-directory)
 
 (require 'd4-pkg)
-(require 'd4-org)
 (when (file-exists-p (concat local-lisp-directory "d4-local.el"))
   (require 'd4-local))
+;; --- org functions
+
+(defun d4-org-min->string (minutes)
+  "convert given minutes to a \"hh:mm\" string
+see: d4-org-string->min (inverse)"
+  (let* ((hs (* 60 60))
+         (ms 60)
+         (h (floor (/ minutes ms)))
+         (m (mod minutes ms)))
+    (format "%d:%02d" h m)))
+
+(defun d4-org-string->min (time)
+  "convert given \"hh:mm\" string to minutes
+see: d4-org-min->string (inverse)"
+  (cl-destructuring-bind (h m)
+     (map 'list 'string-to-number
+          (split-string time ":"))
+     (+ (* 60 h) m)))
+
+(defun d4-org-strange-time->min (strange-time)
+  "convert given 'strange-time' (number 110 for 01:10) to minutes (->
+returns 70 (60 + 10))"
+  (+ (* 60 (floor strange-time 100))
+     (mod strange-time 100)))
+
+(defun d4-org-min-diff (t1 t2)
+  "Time difference in minutes between two time strings in \"hh:mm\"
+format.
+returns a \"hh:mm\" string
+see: d4-org-min->string and d4-org-string->min"
+  (d4-org-min->string
+   (abs (- (d4-org-string->min t1)
+           (d4-org-string->min t2)))))
+
+(defun d4-org-sum (&rest args)
+  "Sum up all given \"hh:mm\" strings.
+returns a \"hh:mm\" string
+see: d4-org-min->string and d4-org-string->min"
+  (d4-org-min->string
+   (reduce '+
+           (map 'list 'd4-org-string->min
+                args))))
+
+(defun d4-org-avg (&rest args)
+  "Average all given \"hh:mm\" strings.
+returns a \"hh:mm\" string
+see: d4-org-min->string and d4-org-string->min"
+  (d4-org-min->string
+   (/ (reduce '+
+              (map 'list 'd4-org-string->min
+                   args))
+      (length args))))
+
 ;; --- custom functions
 
 (defun d4-set-background (mode)
