c3e6e29bbfb71a8c4b78ec94387c533104180d0d
d4ryus <d4ryus@mailbox.org>
2022-08-25 14:50:56 +0200

    emacs: Add popper

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -333,6 +333,41 @@
   (setq rg-executable
         (expand-file-name ".cargo/bin/rg" (getenv "HOME"))))
 
+(use-package popper
+  :ensure t
+  :bind (("C-x /"  . popper-toggle-latest)
+         ("C-x M-/" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$"
+          "\\*Async Shell Command\\*"
+          rg-mode
+          grep-mode
+          help-mode
+          compilation-mode))
+  (setq popper-window-height
+        (lambda (win)
+          (let ((height (floor (frame-height) 3)))
+            (fit-window-to-buffer win height height))))
+  (popper-mode +1)
+  (popper-echo-mode +1))
+
+;; Inject display-buffer-use-some-window by overiding
+;; next-error-no-select from emacs to prevent it from opening up
+;; multiple windows while cycling through errors.
+(defun next-error-no-select (&optional n)
+  "Move point to the next error in the `next-error' buffer and highlight match.
+Prefix arg N says how many error messages to move forwards (or
+backwards, if negative).  Finds and highlights the source line
+like \\[next-error], but does not select the source buffer."
+  (interactive "p")
+  (save-selected-window
+   (let ((next-error-highlight next-error-highlight-no-select)
+         (display-buffer-overriding-action
+           '(display-buffer-use-some-window (inhibit-same-window . t))))
+     (next-error n))))
+
 (defun d4-toggle-trailing-whitespace ()
   (interactive)
   (setq show-trailing-whitespace
