668d6d38df597c151cd64351a0230c6ae2261be2
d4ryus <d4ryus@mailbox.org>
2023-09-03 08:38:46 +0200

    emacs: Treesitter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -345,6 +345,38 @@
 (use-package fzf
   :bind ("C-x p" . fzf))
 
+(use-package treesit
+  :ensure nil
+  :when (treesit-available-p)
+  :init
+  ;; https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))
+        major-mode-remap-alist
+        '((yaml-mode . yaml-ts-mode)
+          (c-mode . c-ts-mode)
+          (c++-mode . c++-ts-mode)
+          (c-or-c++-mode . c-or-c++-ts-mode)
+          (javascript-mode . js-ts-mode)
+          (js-json-mode . json-ts-mode)
+          (css-mode . css-ts-mode)
+          (python-mode . python-ts-mode)
+          (gdscript-mode . gdscript-ts-mode))))
+
 ;; Inject display-buffer-use-some-window by overiding
 ;; next-error-no-select from emacs to prevent it from opening up
 ;; multiple windows while cycling through errors.
