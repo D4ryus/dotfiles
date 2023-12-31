8ce308629e7448859fff6e37b129a02d90b572cf
d4ryus <d4ryus@mailbox.org>
2023-12-31 16:48:04 +0100

    dir-locals: Indentation for cl-destructuring-bind

--- .emacs.d/.dir-locals.el
+++ .emacs.d/.dir-locals.el
@@ -1,4 +1,6 @@
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
+                     (eval . (put 'cl-destructuring-bind 'lisp-indent-function 2))
+                     (eval . (put 'cl-destructuring-bind 'common-lisp-indent-function 2))
                      (eval . (put 'use-package 'lisp-indent-function 1))
                      (eval . (put 'use-package 'common-lisp-indent-function 1)))))
 
