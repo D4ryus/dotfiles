50c4e8d267696fadcab19baa1085d9472739d0d2
d4ryus <d4ryus@mailbox.org>
2023-12-31 16:30:43 +0100

    emacs: Add treesit-auto mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -367,42 +367,12 @@
   ;: File mode specification error: (error No language registered for major mode ‘emacs-lisp-mode’)
   :hook (go-mode . tree-sitter-hl-mode))
 
-(use-package tree-sitter-langs)
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
 
 (use-package fzf
   :bind ("C-x p" . fzf))
-
-(use-package treesit
-  :ensure nil
-  :when (treesit-available-p)
-  :init
-  ;; https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
-  (setq treesit-language-source-alist
-        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-          (cmake "https://github.com/uyha/tree-sitter-cmake")
-          (css "https://github.com/tree-sitter/tree-sitter-css")
-          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-          (go "https://github.com/tree-sitter/tree-sitter-go")
-          (html "https://github.com/tree-sitter/tree-sitter-html")
-          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-          (json "https://github.com/tree-sitter/tree-sitter-json")
-          (make "https://github.com/alemuller/tree-sitter-make")
-          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-          (python "https://github.com/tree-sitter/tree-sitter-python")
-          (toml "https://github.com/tree-sitter/tree-sitter-toml")
-          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))
-        major-mode-remap-alist
-        '((yaml-mode . yaml-ts-mode)
-          (c-mode . c-ts-mode)
-          (c++-mode . c++-ts-mode)
-          (c-or-c++-mode . c-or-c++-ts-mode)
-          (javascript-mode . js-ts-mode)
-          (js-json-mode . json-ts-mode)
-          (css-mode . css-ts-mode)
-          (python-mode . python-ts-mode)
-          (gdscript-mode . gdscript-ts-mode))))
 
 (use-package dtrt-indent
   :custom
