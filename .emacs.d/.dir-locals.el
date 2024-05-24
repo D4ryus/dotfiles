613bdc290a4008ebca78a9b91febb91d612a0e00
d4ryus <d4ryus@mailbox.org>
2024-05-24 09:55:53 +0200

    emacs: Correct indentation for named-let

--- .emacs.d/.dir-locals.el
+++ .emacs.d/.dir-locals.el
@@ -1,6 +1,8 @@
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
                      (eval . (put 'cl-destructuring-bind 'lisp-indent-function 2))
                      (eval . (put 'cl-destructuring-bind 'common-lisp-indent-function 2))
+                     (eval . (put 'named-let 'common-lisp-indent-function 2))
+                     (eval . (put 'named-let 'common-lisp-indent-function 2))
                      (eval . (put 'use-package 'lisp-indent-function 1))
                      (eval . (put 'use-package 'common-lisp-indent-function 1)))))
 
